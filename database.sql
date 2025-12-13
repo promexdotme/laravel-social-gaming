5df3aba71577b47889f90105f6dacabe11c9ef4b
PROMEX.ME <63473041+promexdotme@users.noreply.github.com>
2025-12-12 21:36:54 -0800

    Add usage notes for SQL files
    Added notes about using older SQL file and private repo.

---
+++ database.sql
@@ -0,0 +1,2 @@
+You can safely use the older 10.5.sql file, included in this repo, and included in the Legacy 11 repo.
+  For up2date SQL file check the private Laravel-Gaming-WebSockets Repo [also included in your sponsorship welcome email]
